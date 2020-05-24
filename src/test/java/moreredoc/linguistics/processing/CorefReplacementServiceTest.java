b93c89ed5ea7b25fcee3ff068dbf7bc3db3ff6dd
Andreas Huber <andreas@andhub.de>
2020-05-24 14:55:16 +0200

    Add further assertions for coref replacement service test

--- src/test/java/moreredoc/linguistics/processing/CorefReplacementServiceTest.java
+++ src/test/java/moreredoc/linguistics/processing/CorefReplacementServiceTest.java
@@ -7,10 +7,14 @@ import static org.junit.Assert.assertEquals;
 public class CorefReplacementServiceTest {
 	
 	String unresolvedSentence = "Mike drives a car. The car of him is green.";
+	String unresolvedSentence2 = "Mike likes onions. He cooks regularly.";
+	String unresolvedSentence3 = "Mike lives alone. His pet is named Waldi.";
 
 	@Test
 	public void test() {
 		assertEquals("Mike drives a car . The car of Mike is green . ", CorefReplacementService.resolveCoreferences(unresolvedSentence));
+		assertEquals("Mike likes onions . Mike cooks regularly . ", CorefReplacementService.resolveCoreferences(unresolvedSentence2));
+		assertEquals("Mike lives alone . Mike pet is named Waldi . ", CorefReplacementService.resolveCoreferences(unresolvedSentence3));
 	}
 
 }
